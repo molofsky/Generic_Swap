a2d0c75c185ed4e35162ca0e5c556fe77fe4fbbd
Adrian Molofsky <molofsky@stanford.edu>
2023-01-09 23:20:08 -0800

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,5 +1,5 @@
 /* A generic swap function that swaps two void * arguments
-* of nbyte size. Takes user input through main().
+* of nbyte size. 
 *
 * @author Adrian Molofsky
 * @version 1/9/23
@@ -11,9 +11,3 @@ Void swap(void *data1ptr, void *data2ptr, size_t nbytes) {
 	memcpy(data1ptr, data2ptr, nbytes);
 	memcpy(data2ptr, temp, nbytes)
 } // the nbytes will decide the size of the datatype in a function
-
-
-int main() {
-	
-	return 0;
-}
